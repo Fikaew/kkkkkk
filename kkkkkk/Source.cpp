0f749f1946915ec63181ed44da08b615d473d7f1
FIKAEW CHAYAWATTANA <63010719@KMITL.AC.TH>
2020-09-12 20:30:25 +0700

    Add project files.

---
+++ kkkkkk/Source.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main() 
+{
+	int n, a = 0, b, c, d = 0;
+	scanf_s("%d", &n);
+	while (a < n)
+	{
+		for (b = 1;; b++)
+		{
+			for (c = 1; c <= b; c++)
+			{
+				if (b % c == 0)
+				{
+					d++;
+				}
+			}
+			if (d == 2)
+			{
+				printf("%d ", b);
+				d = 0;
+				a++;
+			}
+			else
+			{
+				d = 0;
+			}
+			if (a >= n)
+			{
+				break;
+			}
+		}
+	}
+}
